e814db36e93dbbf159757daaf36f3c18c351d1bb
Steam Deck User <svetoslavangelov86@gmail.com>
2024-07-06 09:59:52 +0100

    Refactored main and moved gcs related functions

--- main.py
+++ main.py
@@ -2,27 +2,7 @@ import os
 import microphone_stream as ms 
 import pvporcupine
 import wave
-from google.auth import default
-from google.cloud import storage
-from google.oauth2 import service_account
 from dotenv import load_dotenv
-
-def auth_service_account():
-    GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-    credentials = service_account.Credentials.from_service_account_file(GOOGLE_APPLICATION_CREDENTIALS)
-    scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
-    project = credentials.project_id
-    return scoped_credentials, project
-
-def write_to_gcs(bucket: str, source_file: str, destination_blob: str): 
-    credentials, project = auth_service_account()
-    client = storage.Client(project=project, credentials=credentials)
-    bucket = client.bucket(bucket)
-    blob = bucket.blob(destination_blob)
-
-    blob.upload_from_filename(source_file)
-
-    print(f'File {source_file} uploaded to {destination_blob}')
 
 def write_audio_file(filename: str, audio_content: object, sample_rate: int):
     with wave.open(filename, 'wb') as wf: 
