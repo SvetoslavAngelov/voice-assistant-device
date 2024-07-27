84304120d2fbabda6edc1030cde6935a1080331b
Steam Deck User <svetoslavangelov86@gmail.com>
2024-07-27 22:55:06 +0100

    Add: write wav files to memory

---
+++ audio_utils.py
@@ -0,0 +1,21 @@
+import wave
+import io
+
+def write_audio_file(audio_buffer: str, sample_rate: int) -> str:
+    """
+        Takes a byte array string and converts it to a *.wav byte array string. 
+
+        :audio_buffer: byte array string to convert
+        :sample_rate: the sample rate of the conversion
+
+        :returns: converted *.wav file as a byte array string 
+    """ 
+    output = io.BytesIO()
+
+    with wave.open(output, 'wb') as wf: 
+        wf.setnchannels(1)
+        wf.setsamplewidth(2)
+        wf.setframerate(sample_rate)
+        wf.writeframes(audio_buffer)
+
+    return output.getvalue()
